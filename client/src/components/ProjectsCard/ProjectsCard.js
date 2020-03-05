1fab4aab396d30e029d74acdb1ae27dffb490cba
srtalaie <srt4721@yahoo.com>
2020-03-05 13:26:30 -0800

    Cleaning up code in projects card

--- client/src/components/ProjectsCard/ProjectsCard.js
+++ client/src/components/ProjectsCard/ProjectsCard.js
@@ -1,11 +1,5 @@
 import React from "react";
-import Typography from '@material-ui/core/Typography';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
 import { GridListTile, GridListTileBar } from "@material-ui/core";
-import ExpansionPanel from '@material-ui/core/ExpansionPanel';
-import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
-import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 
 const ProjectsCard = ({ image, gitHubLink, title, webLink }) => {
     return (
@@ -16,36 +10,6 @@ const ProjectsCard = ({ image, gitHubLink, title, webLink }) => {
                 subtitle = {<a href={gitHubLink} style={{ color: "white" }}>Repo Link</a>}
             />   
         </GridListTile>
-        // <Card style={cardstyle}>
-        //     <CardActionArea>
-        //         <CardMedia 
-        //             component="img"
-        //             src={image}
-        //             title={title}
-        //             height="10%"
-        //             width="10%"
-        //         />
-        //     </CardActionArea>
-        //     <CardContent style={innerStyle}>
-        //         <Typography gutterBottom variant="h5" component="h4">
-        //             {title}
-        //         </Typography>
-        //         <ExpansionPanel style={innerStyle}>
-        //             <ExpansionPanelSummary>
-        //                     <Typography>Project Description</Typography>
-        //             </ExpansionPanelSummary>
-        //             <ExpansionPanelDetails>
-        //                 <Typography component="p" color="textSecondary">
-        //                     {description}
-        //                 </Typography>
-        //             </ExpansionPanelDetails>
-        //         </ExpansionPanel>
-        //     </CardContent>
-        //     <CardActions>
-        //         <Button size="small" color="secondary" href={gitHubLink}>GitHub Repo</Button>
-        //         <Button size="small" color="primary" href={webLink}>Site</Button>
-        //     </CardActions>
-        // </Card> 
     )
 }
 
